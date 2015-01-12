2d9ef666c3b979ac9edf7765648ea82baacab08b
Chubes <Chubes@andrew-mercers-macbook.local>
2015-01-11 20:21:25 -0500

    Tic Tac Toe: Better

--- TTT.rb
+++ TTT.rb
@@ -1,12 +1,22 @@
 require "pry"
 @table = (1..9).to_a
 @wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3]]
-
+@choices = [1, 2, 3, 4]
 # hash to board index stuff
 
 
 def greeting
   puts "Welcome to Tic Tac Toe"
+  puts "PvP or CPU?"
+  #@choices.each do |x|
+   # gets.chomp.to_i
+  #if x[0]
+    #ttt_game
+  #end
+  #if x[1]
+    #ttt_game_2
+  #end
+ #end   
 end
 
 def print_board
@@ -36,12 +46,14 @@ end
 def cpu_turn
   @table.reject{ |x| x.class == String }
    board = @table
-  if player.even? 
+  if x.even? 
     puts "CPU's turn."
+  else puts "Player's turn"
   end
-  player_choice = gets.chomp.to_i
-  if player.even?  
+  x_choice = gets.chomp.to_i
+  if x.even?  
     board[player_choice - 1] = "O"
+  else puts "X"
   end
   board
 end
@@ -55,10 +67,9 @@ def winner?
       board[2] == board[5] && board[2] == board[8]
   c = board[0] == board[4] && board[0] == board[8] || board[2] == board[4] && board[2] == board[6] 
   if a || b || c
-    puts "Winner!"
+    puts "Winner! Press Return to Exit"
     gets
   end
-  return false
  end
 end
 
@@ -71,18 +82,28 @@ end
 
 def ttt_game
   greeting
-  turn_counter = 1 
-  while turn_counter < 10
+  turn_counter = 9 
+  while turn_counter > 0
     print_board
     table = take_turn(table, turn_counter)
-    turn_counter += 1 
-   
-  if winner? == true
-      then turn counter == 10
-    return false 
+    turn_counter -= 1
+    if winner? == true 
+    return false
     end
-  end 
-end
+    if turn_counter == 0
+      puts "Its a Draw!"
+    end
+  end    
+end 
+
+#def ttt_game_2
+  #turn_counter = 10 
+  #while winner? == false
+    #print_board
+    #table = cpu_turn
+    #turn_counter -= 1
+  #end    
+#end
  
 ttt_game 
 
