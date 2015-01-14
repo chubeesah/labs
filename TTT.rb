45c297a7c9ab6e8955b0911e92893c238921a3ae
Chubes <Chubes@andrew-mercers-macbook.local>
2015-01-14 09:06:12 -0500

    Good Enough for now

--- TTT.rb
+++ TTT.rb
@@ -1,119 +1,106 @@
-require "pry"
+require 'pry'
 
-table = (1..9).to_a
-WINS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 3]]
+TABLE = (1..9).to_a
+WINS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], 
+       [3, 6, 9], [1, 5, 9], [7, 5, 3]]
 # hash to board index stuff
 
-
 def greeting
-  puts "Welcome to Tic Tac Toe" 
+  puts 'Welcome to Tic Tac Toe' 
 end
 
 def choose_mode()
-  puts "#PvP(1) or CPU(2)?"
-  result = gets.chomp
-  if result == 1
-    :human
-  elsif result == 2
-    :cpu
+  puts '#PvP(1) or CPU(2)?'
+  result = gets.chomp.to_i
+    if result == 1 
+      :human  
+    else
+      :cpu
   end
 end
 
 def choose_player
-  puts "Player 1: X or O?"
+  puts 'Player 1: X or O?'
   result = gets.chomp.upcase
-  until ["X", "O"].include?(result)
-    puts "Not a choice. Pick again!"
+  until ['X', 'O'].include?(result)
+    puts 'Not a choice. Pick again!'
     result = gets.chomp.upcase
   end
   result
 end
 
-def print_board(table)
+def print_board
   puts "
-     #{table[0]} | #{table[1]} | #{table[2]}
-     #{table[3]} | #{table[4]} | #{table[5]}
-     #{table[6]} | #{table[7]} | #{table[8]}
-  " 
+     #{TABLE[0]} | #{TABLE[1]} | #{TABLE[2]}
+     #{TABLE[3]} | #{TABLE[4]} | #{TABLE[5]}
+     #{TABLE[6]} | #{TABLE[7]} | #{TABLE[8]}" 
 end
 
-def take_turn(table, turn, player)
+def take_turn(turn, player)
   player_choice = gets.chomp.to_i
-  # the variable player here is 'X' or 'O'
   other_player = player == 'X' ? 'O' : 'X'
   if turn.odd?
     puts "#{player}'s turn."
-    table[player_choice - 1] = player
+    TABLE[player_choice - 1] = player
     return player
   else
     puts "#{other_player}'s turn."
-    table[player_choice - 1] = other_player
+    TABLE[player_choice - 1] = other_player
   end
-  
-  board
 end
 
-def cpu_turn(table, player, turn)
+def cpu_turn(player, turn)
   cpu_player = player == 'X' ? 'O' : 'X'
-  if turn.even
-  available_moves = table.select{ |x| x.is_a?Fixnum }
-  puts "CPU's turn."
-  table[available_moves.sample - 1] = cpu_player
+  if turn.odd?
+    player_choice = gets.chomp.to_i
+    puts "#{player}'s turn."
+    TABLE[player_choice - 1] = player
+    return player
+  else
+    puts "#{cpu_player}'s turn."
+    available_moves = TABLE.select { |x| x.is_a? Fixnum }
+    TABLE[available_moves.sample - 1] = cpu_player
   end
-  table
 end
 
-def winner?(table)
-  board = table
-  WINS.any? do |i| 
-  a = board[0] == board[1] && board[0] == board[2] || board[3] == board[4] && board[3] == board[5] ||
-      board[6] == board[7] && board[6] == board[8]
-  b = board[0] == board[3] && board[0] == board[6] || board[1] == board[4] && board[1] == board[7] ||
-      board[2] == board[5] && board[2] == board[8]
-  c = board[0] == board[4] && board[0] == board[8] || board[2] == board[4] && board[2] == board[6] 
-  if a || b || c
+def winner?
+  board = TABLE
+  WINS.any? do |i|
+  a = board[0] == board[1] && board[0] == board[2] || board[3] == board[4] && board[3] == board[5] || 
+  board[6] == board[7] && board[6] == board[8]  
+  b = board[0] == board[3] && board[0] == board[6] || board[1] == board[4] && board[1] == board[7] || 
+  board[2] == board[5] && board[2] == board[8]
+  c = board[0] == board[4] && board[0] == board[8] || board[6] == board[4] && board[6] == board[2]  
+    if a || b || c
     puts "Winner! Press Return to Exit"
-    print_board(board)
+    print_board
     gets
+    end
   end
- end
 end
 
-def ttt_game(table)
+def ttt_game
   greeting
   turn_counter = 9
   player1 = choose_player
   mode = choose_mode 
-  if mode == :human
-      board = take_turn(table, turn_counter, player1)
-      turn_count -= 1
-  elsif mode == :cpu
-      cpu_turn(table, player1, turn)
+    until winner?
+      print_board
+      if mode == :human
+      take_turn(turn_counter, player1)
       turn_counter -= 1
-  end# TODO: write a choose mode function!
-  until winner?(table)
-    print_board(table)
-    # TODO: Fix this logic.
-    if winner?(table) == true 
-      return false
-    end
-    if turn_counter == 0
-      print_board(table)
+      elsif mode == :cpu
+      cpu_turn(player1, turn_counter)
+      turn_counter -= 1
+      end
+      if turn_counter == 0
+      print_board
       puts "Its a Draw!"
-    end
+      return false
+      end
   end
 end 
 
-#def ttt_game_2
-  #turn_counter = 10 
-  #while winner? == false
-    #print_board
-    #table = cpu_turn
-    #turn_counter -= 1
-  #end    
-#end
  
-ttt_game(table)
-
-  #cpu needs to be able to select different corresponding number and replace with O
+ttt_game
   #easy, medium, hard, legendary	 
